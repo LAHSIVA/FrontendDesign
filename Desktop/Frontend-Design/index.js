a28f43cfdd04a19cd53e1bea3e7f276ab1064df1
LAHSIVA <vishalvenkat2019@gmail.com>
2024-07-18 11:24:34 +0530

    Created Read operation

--- Desktop/Frontend-Design/index.js
+++ Desktop/Frontend-Design/index.js
@@ -4,10 +4,35 @@ const app=express();
 const PORT=3000;
 
 app.all(`/`,(req,res)=>{
-    console.log(`Request >`,req);
-    console.log(`Response >`,res);
+    // console.log(`Request >`,req);
+    // console.log(`Response >`,res);
     res.send(`Hello Express World!`);
 });
+
+const todos=[{
+    id:'1',
+    title:'Task1',
+    completed:'false'
+},{
+    id:'2',
+    title:'Task2',
+    completed:'true'
+}];
+
+//Read
+
+app.get('/todos',(req,res)=>{
+    res.json(todos);
+
+});
+
+//Create
+
+//Update
+
+//Delete
+
+
 
 app.listen(PORT,()=>{
     console.log(`Service is Running at Port ${PORT}`);
